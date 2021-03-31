762174b0e0e1e712ac05523f0fef426b7f1010de
Céline LUDIG <cludig@splio.com>
2021-03-31 09:19:24 +0200

    Added go exercise

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tech Test Fullstask Marketing Automation
 
+
+## Writing back end code
+### Algo  : (main.go)
+See the guidelines in the code directly. This exercice can be done in another language of your choice.  
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+// Un coureur parie avec ses amis qu'il finira le prochain marathon,
+// ses amis lui propose de lui donner 1000 euros s'il arrive dans les 100 premiers coureurs et
+// s'il finit le marathon, il gagnera 100 euros.
+// Cependant, après le 10 000ème coureur, on considère qu'il n'a pas terminé.
+
+// startPosition représente la position de départ
+// input représente un tableau avec 2 nombres indiquant respectivement, pour chaque kilomètre,
+// le nombre de coureurs l'ayant dépassé et le nombre de coureurs qu'il a dépassé.
+
+// en sortie, on attend le montant du gain ou 'KO' s'il n'a pas fini.
+
+// NB : L'exercice peut être fait dans un autre langage que le go.
+
+func main() {
+	startPosition := 1288
+	input := []string{
+		"114 8",
+		"137 19",
+		"104 9",
+		"38 13",
+		"140 10",
+		"28 17",
+		"40 8",
+		"18 5",
+		"141 1",
+		"5 0",
+		"15 17",
+		"106 15",
+		"120 18",
+		"88 2",
+		"54 3",
+		"134 17",
+		"70 12",
+		"22 15",
+		"56 12",
+		"104 10",
+		"143 6",
+		"108 8",
+		"154 1",
+		"64 9",
+		"81 17",
+		"88 3",
+		"121 2",
+		"77 10",
+		"82 6",
+		"34 6",
+		"25 12",
+		"79 2",
+		"46 8",
+		"86 13",
+		"120 12",
+		"40 8",
+		"26 12",
+		"71 19",
+		"71 18",
+		"57 1",
+		"54 1",
+		"144 16",
+	}
+
+	// place your code here
+
+}
